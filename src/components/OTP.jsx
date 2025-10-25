a608a187644bec37a481412d879e7f794fa96fea
Shruti <164215234+saoirseros@users.noreply.github.com>
2025-10-26 01:57:13 +0530

    updated Otp.jsx

--- src/components/OTP.jsx
+++ src/components/OTP.jsx
@@ -1,19 +1,14 @@
-// useState, useRef, useEffect are hooks provided by react. Think of them as utility functions. More on react hooks - https://daily.dev/blog/react-hooks-explained-simply
+
 import { useState, useRef, useEffect } from "react";
 import "../styles.css";
 
-// We are creating a OTP component with a prop otpLength
-// Components in react are modular pieces of code that can be used to stitch together the entire web page
-// Components are essentially functions that run and return the HTML from them so similar to function they can also take arguments called props
-// More on react components - https://medium.com/@reactmasters.in/what-are-components-and-types-of-components-in-react-js-4e2642b136a2
+
 export default function OTP({ otpLength = 6 }) {
   const [otpFields, setOtpFields] = useState(new Array(otpLength).fill(""));
   const ref = useRef([]);
 
   function handleKeyDown(e, index) {
-    // handleKeyDown event gets triggered every single time a key is pressed when the focus is on an input
-    const key = e.key; // This tells us which keyboard key has been pressed
-    // console.log(key, e);
+    const key = e.key; 
 
     if (key === "ArrowLeft") {
       if (index > 0) ref.current[index - 1].focus();
@@ -25,11 +20,8 @@ export default function OTP({ otpLength = 6 }) {
       return;
     }
 
-    // Make a copy of the current array
     const copyOtpFields = [...otpFields];
-
-    // Let's check if the key is backspace
-    // and delete the digit in that scenario
+    
     if (key === "Backspace") {
       copyOtpFields[index] = "";
       setOtpFields(copyOtpFields);
@@ -37,34 +29,26 @@ export default function OTP({ otpLength = 6 }) {
       if (index > 0) ref.current[index - 1].focus();
     }
 
-    // We should allow single digit entry only
     if (!/^\d$/.test(key)) {
-      // We check if the pressed key is not a number
-      // we simply return from this function
-      // as only numbers are allowed!
       return;
     }
 
     copyOtpFields[index] = key;
     setOtpFields(copyOtpFields);
 
-    // use ref to focus on the next input box when an input is done
     if (index + 1 < otpFields.length) ref.current[index + 1].focus();
   }
 
   function handlePaste(e) {
-    // handlePaste event handles the case where the user pastes a certain value inside the input
-    e.preventDefault(); // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+    e.preventDefault(); 
 
     const pastedData = e.clipboardData.getData("text");
-    const digits = pastedData.match(/\d/g); // We are using this regex to extract the digits from the pasted data
+    const digits = pastedData.match(/\d/g); 
 
     if (!digits) return;
 
     const nextOtp = otpFields.slice();
 
-    // In  our current implementation
-    // we will always paste from the beginning!!
     for (let i = 0; i < otpLength && digits.length; ++i) {
       nextOtp[i] = digits[i];
     }
